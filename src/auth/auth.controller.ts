ea1d2b7c657e94740bb3519266ddeb199b13ba82
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-07 10:47:58 +0200

    add base auth logic & start work with e2e tests

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log({ dto });
+    return this.authService.signup(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,30 @@
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { Test } from '@nestjs/testing';
+
+describe('App e2e', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true }),
+    );
+
+    await app.init();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  it.todo('should pass');
+});
